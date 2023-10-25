085d4eb4778c84003fcc36dfafe337eae789bacc
kavya191 <kavyabose191@gmail.com>
2023-10-25 22:51:35 +0530

    quiz app in react

---
+++ src/api/quizData.js
@@ -0,0 +1,66 @@
+const quizData = {
+    questions: [
+        {
+            question: "what will <code>console.log('Hello World')</code> log to the console?",
+            answers: [
+                "'Hello World'",
+                "Hello World",
+                "String",
+                "None of the above",
+            ],
+            correctAnswerIndex: 1,
+        },
+        {
+            question: "what will <code>console.log(15 + 5)</code> log to the console?",
+            answers: [
+                "155",
+                "'155'",
+                "20",
+                "'20'",
+            ],
+            correctAnswerIndex: 2,
+        },
+        {
+            question: "what will <code>console.log('15 + 20')</code> log to the console?",
+            answers: [
+                "35",
+                "1520",
+                "'35'",
+                "15 + 20",
+            ],
+            correctAnswerIndex: 3,
+        }, {
+            question: " Arrays in JavaScript are defined by which of the following statements?",
+            answers: [
+                "It is an ordered list of values",
+                "It is an ordered list of objects",
+                "It is an ordered list of string",
+                "It is an ordered list of functions"
+            ],
+            correctAnswerIndex: 0
+        },
+        {
+            question: "Which of the following can be used to call a JavaScript Code Snippet?",
+            answers: [
+                "Function/Method",
+                "Preprocessor",
+                "Triggering Event",
+                "RMI"
+            ],
+            correctAnswerIndex: 0
+        },
+        {
+            question: "what will <code>console.log(15)</code> log to the console?",
+            answers: [
+                "Number",
+                "'15'",
+                "15",
+                "None of the above",
+            ],
+            correctAnswerIndex: 2,
+        }
+    ],
+
+}
+
+export default quizData;
